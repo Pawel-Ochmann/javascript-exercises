b6e8bcb3f527da460060925bcc8060a189e2cd2f
Pawel Ochmann <pawelochmann@poczta.onet.pl>
2023-03-22 18:45:48 +0100

    Fourth task done. Now check solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(array,par1,par2,par3,par4){   
+    par1 = par1 || undefined;
+    par2 = par2 || undefined;
+    par3 = par3 || undefined;
+    par4 = par4 || undefined;
 
+    const arrayRemoved = [];
+    for (item of array) {
+        if (
+            item===par1 ||
+            item===par2 ||
+            item===par3 ||
+            item===par4 )
+           { continue }
+        arrayRemoved.push(item);
+    }
+    return arrayRemoved;
 };
 
 // Do not edit below this line
