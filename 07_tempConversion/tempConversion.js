2e769e186e5976d4df86a1d5a107cba07c6e3f11
Pawel Ochmann <pawelochmann@poczta.onet.pl>
2023-03-23 18:36:17 +0100

    Do task, important information - for some reason toFixed returns a string, deal with it adding plus

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fDeg) {
+
+  fDeg = (fDeg-32)*(5/9);
+
+  if (fDeg===0 || fDeg%1===0) return fDeg;
+  return +fDeg.toFixed(1)
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cDeg) {
+
+  cDeg = cDeg*(9/5)+32;
+  
+  if (cDeg===0 || cDeg%1===0) return cDeg;
+  return +cDeg.toFixed(1)
 };
 
+console.log(convertToCelsius(-10));
+console.log(convertToFahrenheit(100));
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
