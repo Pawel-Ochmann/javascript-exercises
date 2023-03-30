a254e0c1a2a453b7bf05cca3fceee249bedbd8ae
Pawel Ochmann <pawelochmann@poczta.onet.pl>
2023-03-30 20:59:58 +0200

    I dont think this is the most efficent way to do this, but it works.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,25 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const arr = string.toUpperCase().split('');
+   let filteredArr =  arr.filter(e=> { 
+  if (e==='.' || e===' '||e===','||e==='!') return false;
+  else return true;
+})
+    
+   let reversedArr = [];
+    for (let i=filteredArr.length-1; i>=0; i--) {
+     reversedArr.push(filteredArr[i])
+    }
+    let test = true;
+    for (i=0; i<reversedArr.length; i++) {
+        if (reversedArr[i]!==filteredArr[i]) {
+        test = false;
+        break;
+        }
+    }
+    return test;
 };
 
+console.log(palindromes('Animal loots foliated detail of stool lamina.'))
 // Do not edit below this line
 module.exports = palindromes;
+
