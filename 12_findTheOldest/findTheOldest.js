2df1043cc9901f9a2f1bef049a3855d774f44106
Pawel Ochmann <pawelochmann@poczta.onet.pl>
2023-03-31 12:50:38 +0200

    Task done using reduce method, and new Data object

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,32 @@
-const findTheOldest = function() {
+const people = [
+    {
+      name: "Carly",
+      yearOfBirth: 2018,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+  ]
 
+const actualYear = new Date().getFullYear()
+
+const findTheOldest = function(people) {
+    const theOldest = people.reduce((oldest,person)=> {
+        if(!oldest.yearOfBirth) return person;
+        let actualAge = (oldest.yearOfDeath || actualYear)- oldest.yearOfBirth;
+        let personAge = (person.yearOfDeath || actualYear)- person.yearOfBirth;
+        if (personAge>actualAge) return person;
+        else return oldest;
+    },{})
+    return theOldest;
 };
-
+console.log(findTheOldest(people));
 // Do not edit below this line
 module.exports = findTheOldest;
