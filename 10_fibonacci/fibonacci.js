d5342813313c2909ab12788833bd8ec616541b53
Pawel Ochmann <pawelochmann@poczta.onet.pl>
2023-03-31 12:21:59 +0200

    Fibonacci done, using recursive method

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,9 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+    if(a<0) return 'OOPS';
+    if ((+a-2)<=0) return 1;
+    return fibonacci(+a-1)+fibonacci(+a-2)
 };
 
 // Do not edit below this line
 module.exports = fibonacci;
+console.log(fibonacci(5))
